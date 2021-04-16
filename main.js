339d63665813e33fd1c37542eae37c9fac2f8373
mrk-lwrnc <mark016.lawrence.feliciano@gmail.com>
2021-04-16 18:29:22 +0800

    Revert "updated scripts"
    This reverts commit 54172ea3ef1e15302f410afd919e265432517555.

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 const time_selector = document.querySelector('.time-container');
 const date_selector = document.querySelector('.date-container');
 
-const interval = setInterval(updateDateTimeDisplay, 1000);
+const interval = setInterval(updateDateTimeDisplay, 2000);
 
 function updateDateTimeDisplay() {
     const current_date = new Date();
@@ -18,15 +18,14 @@ function updateDateTimeDisplay() {
         day: 'numeric'
     });
 
-    if (time_selector.textContent !== time) {
-        time_selector.textContent = time;
-        updateDisplayStyle(current_date.getHours());
-    }
-
+    if (time_selector.textContent !== time) { time_selector.textContent = time; }
     if (date_selector.textContent !== day_and_date) { date_selector.textContent = day_and_date; }
+
+    updateDisplayStyle(current_date);
 }
 
-function updateDisplayStyle(current_hour) {
+function updateDisplayStyle(current_date) {
+    const current_hour = current_date.getHours();
     const background_colors = [
         '#111111', '#333333', '#555555',
         '#777777', '#999999', '#bbbbbb',
@@ -37,12 +36,9 @@ function updateDisplayStyle(current_hour) {
         '#666666', '#555555', '#444444',
         '#333333', '#222222', '#111111',
     ];
-    const LIGHT_MODE_MINIMUM = 7;
-    const LIGHT_MODE_MAXIMUM = 10;
 
-    if (current_hour >= LIGHT_MODE_MINIMUM && current_hour <= LIGHT_MODE_MAXIMUM) {
-        document.body.style.color = '#000000';
-    } else { document.body.style.color = '#ffffff'; }
+    if (current_hour >= 7 && current_hour <= 11) { document.body.style.color = '#000000'; }
+    else { document.body.style.color = '#ffffff'; }
 
     document.body.style.backgroundColor = background_colors[current_hour];
 }
